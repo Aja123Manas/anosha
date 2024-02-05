b82f7bfeb2cdc43601893814023a93836cdce946
Brian254murithi <brianmurimi120@gmail.com>
2024-02-05 16:48:46 +0300

    initial commit

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# Variable is a container to store a value
+school = "eMobilis"
+firstname = "Kezekia"
+lastname = "Manas"
+x = 5
+y = 10
+print("The sum of ",x,"and",y,"is",x+y)
+print("My fullname is",firstname,lastname)
+print("I study at",school)
+print(x + y)
+print(school)
+print(firstname)
+print(lastname)
